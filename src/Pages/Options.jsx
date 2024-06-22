ce766238e05449fe2107bddbcff779c2e2357060
Krassusky <barroskrassusky@gmail.com>
2024-06-22 13:16:22 -0300

    options updated

--- src/Pages/Options.jsx
+++ src/Pages/Options.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const Options = () => {
     const { storeId } = useParams();
 
-    // Replace this with actual logic to fetch data based on storeId if needed
-
     const handleOptionSelect = (option) => {
-       
-       
-       
-      
-       
         console.log(`${option} selecionada`); // Log to console the selected option
-        // Add logic here for further actions based on selected option
     };
 
     return (
         <div>
             <h1>Loja {storeId}</h1> {/* Display the store ID in the title */}
             <div>
-                <button onClick={() => handleOptionSelect('Câmara')}>Câmara</button>
-                <button onClick={() => handleOptionSelect('Gôndola')}>Gôndola</button>
+                {/* Use Link to navigate to Count page with parameters */}
+                <Link to={`/Count?storeId=${storeId}&selectedOption=Câmara`}>
+                    <button onClick={() => handleOptionSelect('Câmara')}>Câmara</button>
+                </Link>
+                <Link to={`/Count?storeId=${storeId}&selectedOption=Gôndola`}>
+                    <button onClick={() => handleOptionSelect('Gôndola')}>Gôndola</button>
+                </Link>
             </div>
         </div>
     );
